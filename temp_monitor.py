d3d429e454c70ba3bda259b86a9949b0ee8fe3a3
yukimoritani <yuuki.moritani@gmail.com>
2026-01-12 11:53:52 +0900

    Update through the test.

--- temp_monitor.py
+++ temp_monitor.py
@@ -30,6 +30,21 @@ def get_option():
 #                           help='Whether to draw learning curve after learning')
     return argparser.parse_args()
 
+def create_filename():
+
+    now_utc = datetime.now(timezone.utc)
+    now_utc_str = now_utc.strftime("%Y%m%d_%H%M%S")
+    logname = f"tempLog_{now_utc_str}.csv"
+
+    return logname
+
+def write_header(fout):
+
+    print("UTC", file=fout, end='')
+    for i, (chk, chv) in enumerate(config_data['sensor'][0].items()):
+        print(f",{chk}_{chv}", file=fout, end='')
+    print('', file=fout)  # change lines
+
 
 if __name__ == '__main__':
 
@@ -37,15 +52,12 @@ if __name__ == '__main__':
 
     # set logfile name
     if args.outLog is None:
-        now_utc = datetime.now(timezone.utc)
-        now_utc_str = now_utc.strftime("%Y%m%d_%H%M%S")
-        logname = f"tempLog_{now_utc_str}.csv"
+        logname=create_filename()
     else:
         logname = args.outLog
 
     with open('config.yaml', 'r') as file:
         config_data = yaml.safe_load(file)
-
 
     hostips=config_data['adam']['hostip']
     port=config_data['adam']['port']
@@ -53,25 +65,32 @@ if __name__ == '__main__':
     tempmon=tempsLogger.temps('logger1', hostips, port)
 
     fout = open(logname, 'w')
-
-    print("UTC", file=fout, end='')
-    for i, (chk, chv) in enumerate(config_data['sensor'][0].items()):
-        print(f",{chk}_{chv}", file=fout, end='')
-    print('', file=fout)  # change lines
+    write_header(fout)
 
     for i in range(0,args.nSample):
         # list of read temperature
-        temps = tempmon.query()
+        try:
+            temps = tempmon.query()
+        except TimeoutError:
+            continue 
 
         now_utc = datetime.now(timezone.utc)
         #now_utc = time.time()
         now_utc_str = now_utc.strftime("%Y-%m-%d %H:%M:%S")
 
-        print(f"{now_utc_str}", file=fout, end='')
-        for i, t in enumerate(temps):
-            print(f"{t:6.2f}", file=fout, end='')
-        print('', file=fout)  # change lines
+        print(f"{now_utc_str}", file=fout, end='', flush=True)
+        for j, t in enumerate(temps):
+            print(f",{t:6.3f}", file=fout, end='', flush=True)
+        print('', file=fout, flush=True)  # change lines
 
         time.sleep(args.cadence)
+
+        # change logfile
+        if i%5000 == 4999:
+
+            fout.close()
+            logname = create_filename()
+            fout = open(logname, 'w')
+            write_header(fout)
 
     fout.close()
